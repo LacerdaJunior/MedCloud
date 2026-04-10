93e38cd5db2cdc5b21adbfc5d5a337bb9b0f9f54
Guilherme Lacerda <felipesaturnino91@gmail.com>
2026-04-09 22:31:11 -0300

    feat: initial project setup with clean architeture and docker infrasctructure(WIP)

---
+++ backend/src/infrastructure/database/connection.js
@@ -0,0 +1,11 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  host: process.env.POSTGRES_HOST || "localhost",
+  user: process.env.POSTGRES_USER || "admin",
+  password: process.env.POSTGRES_PASSWORD || "adminpassword",
+  database: process.env.POSTGRES_DB || "medcloud_db",
+  port: process.env.POSTGRES_PORT || 5432,
+});
+
+module.exports = pool;
