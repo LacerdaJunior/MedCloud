ffb4e00882c7af06bc3f3e9c153f93f8735e8da9
Guilherme Lacerda <felipesaturnino91@gmail.com>
2026-04-17 09:16:32 -0300

    feat(errors): adding global middleware on app.js with express 5

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const userRoutes = require("./src/infrastructure/http/routes/user.routes");
 const sessionRoutes = require("./src/infrastructure/http/routes/session.routes");
 const appointmentsRoutes = require("./src/infrastructure/http/routes/appointments.routes");
+const AppError = require("./src/errors/AppError");
 
 const app = express();
 app.use(express.json());
@@ -13,5 +14,19 @@ app.use("/profile", userRoutes);
 app.use("/appointments", appointmentsRoutes);
 
 //----------------------------- ---------------------------------------------------------
+app.use((error, request, response, next) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: "error",
+      message: error.message,
+    });
+  }
 
+  console.error("❌ Erro Crítico:", error);
+
+  return response.status(500).json({
+    status: "error",
+    message: "Erro interno do servidor",
+  });
+});
 module.exports = app;
