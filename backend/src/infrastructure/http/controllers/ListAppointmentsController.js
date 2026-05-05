61be8fa4a13aa471af86d658485c829951128cba
Guilherme Lacerda <felipesaturnino91@gmail.com>
2026-05-05 09:05:30 -0300

    WIP(list Appointments) add variable page for future feats

--- backend/src/infrastructure/http/controllers/ListAppointmentsController.js
+++ backend/src/infrastructure/http/controllers/ListAppointmentsController.js
@@ -4,14 +4,20 @@ const AppointmentsRepository = require("../../repositories/AppointmentsRepositor
 class ListAppointmentsController {
   async handle(request, response) {
     const { id, role } = request.user;
-    const { status } = request.query;
+    const { status, page = 1 } = request.query;
+
     const appointmentsRepository = new AppointmentsRepository();
 
     const listAppointmentsUseCase = new ListAppointmentsUseCase(
       appointmentsRepository,
     );
 
-    const result = await listAppointmentsUseCase.execute({userId: id, role, status});
+    const result = await listAppointmentsUseCase.execute({
+      userId: id,
+      role,
+      status,
+      page,
+    });
 
     return response.status(200).json(result);
   }
